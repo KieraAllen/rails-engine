eae7815c3c5ea4b3a6bb16b235561d5ee1701c47
Kiera Allen <kieraaallen@gmail.com>
2020-12-15 08:57:33 -0700

    Import data for items table and pgdump tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+# before running "rake db: seed", do the following:
+# - put the "rails-engine-development-pgdump" file in the db/data/
+# - put the "items.csv" file in db/data
+
+cmd = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails-engine_development db/data/rails-engine-development.pgdump"
+puts "Loading PostgreSQL Data dump into local database with command:"
+puts cmd
+system(cmd)
+
+# TO DO
+# - Import the CSV data into the Items tables
+
+filename = "db/data/items.csv"
+
+csv = CSV.foreach(Rails.root.join(filename), :headers => true) do |row|
+  row["unit_price"] = (row["unit_price"].to_f)/100.round(2)
+  Item.create(row.to_hash)
+end
+
+# - Add code to reset primary key sequences on all 6 tables (merchants, items, customers, invoices, invoice_items, transactions)
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
