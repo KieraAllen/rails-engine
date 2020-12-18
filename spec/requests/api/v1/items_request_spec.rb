c5cef381978e9237ee5bd920c2f1cfee43c8beb2
Kiera Allen <kieraaallen@gmail.com>
2020-12-18 07:48:57 -0700

    IN PROGRESS

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -64,10 +64,10 @@ describe "Items API" do
     created_item = Item.last
 
     expect(response).to be_successful
-    expect(created_item.name).to eq(item_params[:name])
-    expect(created_item.description).to eq(item_params[:description])
-    expect(created_item.unit_price).to eq(item_params[:unit_price])
-    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+    expect(created_item.name).to eq(item_params[:attribues][:name])
+    expect(created_item.description).to eq(item_params[:attribues][:description])
+    expect(created_item.unit_price).to eq(item_params[:attribues][:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:attribues][:merchant_id])
   end
 
   it 'can update an existing item' do
